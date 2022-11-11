413c68ff08c2726ecced70777f6031eda08a7c6d
mucahitarslaan <mucahitarslaan@gmail.com>
2022-11-11 23:45:11 +0300

    update

--- Java2022-4/Devs/src/main/java/com/mucahitarslan/Devs/entities/concretes/ProgrammingLanguage.java
+++ Java2022-4/Devs/src/main/java/com/mucahitarslan/Devs/entities/concretes/ProgrammingLanguage.java
@@ -17,6 +17,11 @@ public class ProgrammingLanguage {
         return id;
     }
 
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
