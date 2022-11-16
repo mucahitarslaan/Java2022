0c466e87cf73b330cccba491d2a66d76b5f1b7ae
mucahitarslaan <mucahitarslaan@gmail.com>
2022-11-16 03:31:38 +0300

    update

---
+++ Java2022-5/Devs/src/test/java/com/mucahitarslan/Devs/DevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mucahitarslan.Devs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
